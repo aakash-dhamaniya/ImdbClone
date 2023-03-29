a529d32b9b1e7778e2b25fa94075628ab5b2a3aa
aakash-dhamaniya <aakashdhamaniya.dev@gmail.com>
2023-03-29 10:10:14 +0530

    final product with fixes

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -1,15 +1,15 @@
 "use client";
-export default function error({ error, reset }) {
+
+import { useEffect } from "react";
+export default function Error({ error, reset }) {
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
   return (
     <div className="text-center mt-10">
       <h1>Something went wrong</h1>
-      <button
-        className="hover:text-amber-600"
-        onClick={() => {
-          reset();
-        }}
-      >
-        try again
+      <button className="hover:text-amber-600" onClick={() => reset()}>
+        Try Again
       </button>
     </div>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,12 @@
+export const dynamic = "force-dynamic"; // this is the fix
+
 import Results from "@/components/Results";
+
 const API_KEY = process.env.API_KEY;
+
 export default async function Home({ searchParams }) {
   const genre = searchParams.genre || "fetchTrending";
+
   const res = await fetch(
     `https://api.themoviedb.org/3/${
       genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
@@ -12,8 +17,11 @@ export default async function Home({ searchParams }) {
   if (!res.ok) {
     throw new Error("Failed to fetch data"); // this will be caught by the error page and passed to the page as props
   }
+
   const data = await res.json();
+
   const results = data.results;
+
   return (
     <div>
       <Results results={results} />
